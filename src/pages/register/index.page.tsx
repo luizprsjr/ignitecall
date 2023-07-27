ca698b6635c6ba753e0468498d74ec9a248acc42
Luiz Pires <lpiresjunior@gmail.com>
2023-07-27 02:02:26 -0300

    feat: create register page

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,38 @@
+import { ArrowRight } from 'phosphor-react'
+
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+
+import { Container, Form, Header } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para cria seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+        </label>
+
+        <label>
+          <Text size="sm">Nome de completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
